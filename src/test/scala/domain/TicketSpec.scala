baf3ce13f03e9c5a80b7654c131511802dd56298
インターンシップ05 <internship05@internship05.local>
2019-08-26 16:36:47 +0900

    clean: 誕生日を固定した

--- src/test/scala/domain/TicketSpec.scala
+++ src/test/scala/domain/TicketSpec.scala
@@ -10,25 +10,25 @@ class TicketSpec extends FreeSpec with Matchers {
   "Ticket" - {
     "Holiday" - {
       "CinemaCitizen" in {
-        val customer = Customer(LocalDate.now().minusYears(20), Seq(CinemaCitizen))
+        val customer = Customer(LocalDate.of(1999, 6, 1), Seq(CinemaCitizen))
         val ticket   = Ticket(customer, LocalDateTime.of(2019, 7, 14, 12, 0))
         ticket.plan shouldBe Plan.CinemaCitizen
         ticket.price shouldBe Price(1300)
       }
       "CinemaSenior" in {
-        val customer = Customer(LocalDate.now().minusYears(61), Seq(CinemaCitizen))
+        val customer = Customer(LocalDate.of(1959, 6, 1), Seq(CinemaCitizen))
         val ticket   = Ticket(customer, LocalDateTime.of(2019, 7, 14, 12, 0))
         ticket.plan shouldBe Plan.CinemaSenior
         ticket.price shouldBe Price(1000)
       }
       "No discount" in {
-        val customer = Customer(LocalDate.now().minusYears(20), Seq())
+        val customer = Customer(LocalDate.of(1999, 6, 1), Seq())
         val ticket   = Ticket(customer, LocalDateTime.of(2019, 7, 14, 12, 0))
         ticket.plan shouldBe Plan.Adult
         ticket.price shouldBe Price(1800)
       }
       "Many Certificates" in {
-        val customer = Customer(LocalDate.now().minusYears(17), Seq(HighSchoolStudent, CinemaCitizen, MICard, DisabilityCertificate))
+        val customer = Customer(LocalDate.of(2002, 6, 1), Seq(HighSchoolStudent, CinemaCitizen, MICard, DisabilityCertificate))
         val ticket   = Ticket(customer, LocalDateTime.of(2019, 7, 14, 12, 0))
         ticket.plan shouldBe Plan.DisabilityStudent
         ticket.price shouldBe Price(900)
